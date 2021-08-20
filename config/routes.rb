937d3e18bf2a9e45928ed886468912cd169fb0a0
hrk0930 <a08062151874@icloud.com>
2021-08-20 16:50:44 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'menus/index'
+  root to: "tops#index"
+  resources :menus
+  resources :users,    only: [:show, :edit, :update, :destroy]
+  end
